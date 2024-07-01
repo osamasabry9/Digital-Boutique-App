78f3dc7de31d9de65822d96402196072fc692605
osamasabry9 <osamasabry324@gmail.com>
2024-07-01 21:30:03 +0300

    Added project in repository

---
+++ lib/app/digital_boutique_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DigitalBoutiqueApp extends StatelessWidget {
+  const DigitalBoutiqueApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'app/digital_boutique_app.dart';
+
+void main() {
+  runApp(const DigitalBoutiqueApp());
+}
