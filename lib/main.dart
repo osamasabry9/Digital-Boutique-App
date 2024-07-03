9d3c0cc177bafdfc3c8d3403a45491e724428b3a
osamasabry9 <osamasabry324@gmail.com>
2024-07-04 01:24:03 +0300

    Add SystemChrome for landscape mode support and update dio to v5.5.0
    - Added SystemChrome to allow app rotation to landscape mode in main function
    - Updated dio package to version 5.5.0 for latest features and improvements

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:digital_boutique/app/env.variables.dart';
 import 'package:digital_boutique/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   // to initialize environment variables 
@@ -12,6 +13,14 @@ void main() async {
   await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
   // to initialize firebase app in the main function
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // to run the app
-  runApp(const DigitalBoutiqueApp());
+/* 
+   To allow the app to rotate to landscape mode when the device is
+   in portrait mode and vice versa in the main function. 
+*/
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  ).then((_) {
+    // to run the app
+    runApp(const DigitalBoutiqueApp());
+  });
 }
