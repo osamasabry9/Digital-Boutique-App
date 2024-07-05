329a91bc4a4bef8b0c7d3dae334cbb83a37ec704
osamasabry9 <osamasabry324@gmail.com>
2024-07-05 21:30:04 +0300

    Add dark and light theme configurations
    Added dark and light theme configurations with color definitions for the
    Digital Boutique app. This includes theme creation functions, color
    extensions, and color definitions for both dark and light themes.

---
+++ lib/core/style/theme/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:digital_boutique/core/style/colors/colors_dark.dart';
+import 'package:digital_boutique/core/style/colors/colors_light.dart';
+import 'package:digital_boutique/core/style/theme/color_extension.dart';
+import 'package:flutter/material.dart';
+
+ThemeData themeDark() {
+  return ThemeData(
+    scaffoldBackgroundColor: ColorsDark.mainColor,
+    extensions: const <ThemeExtension<dynamic>>[AppColors.dark],
+    useMaterial3: true,
+   
+  );
+}
+
+ThemeData themeLight() {
+  return ThemeData(
+    scaffoldBackgroundColor: ColorsLight.mainColor,
+    extensions: const <ThemeExtension<dynamic>>[AppColors.light],
+    useMaterial3: true,
+  );
+}
