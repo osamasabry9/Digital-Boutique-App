cf7410ef3dc0d77659f6863aa742006ffc02a73e
osamasabry9 <osamasabry324@gmail.com>
2024-07-06 20:27:32 +0300

    Add new images and theme extensions
    Added new images to the project and created theme extensions for dark and light themes.

---
+++ lib/core/style/images/app_images.dart
@@ -0,0 +1,125 @@
+class AppImages {
+  AppImages._();
+
+  /// Assets for AdminCategoriesDrawer
+  /// assets/images/admin/categories_drawer.png
+  static const String adminCategoriesDrawer =
+      'assets/images/admin/categories_drawer.png';
+
+  /// Assets for AdminDrawerImage
+  /// assets/images/admin/drawer_image.png
+  static const String adminDrawerImage = 'assets/images/admin/drawer_image.png';
+
+  /// Assets for AdminProductsDrawer
+  /// assets/images/admin/products_drawer.png
+  static const String adminProductsDrawer =
+      'assets/images/admin/products_drawer.png';
+
+  /// Assets for AdminUsersDrawer
+  /// assets/images/admin/users_drawer.png
+  static const String adminUsersDrawer = 'assets/images/admin/users_drawer.png';
+
+  /// Assets for CoreEmptyScreen
+  /// assets/images/core/empty_screen.png
+  static const String emptyScreen = 'assets/images/core/empty_screen.png';
+
+  /// Assets for CoreNoNetwork0
+  /// assets/images/core/no_network.gif
+  static const String noNetwork = 'assets/images/core/no_network.gif';
+
+  /// Assets for CorePageUnderBuild
+  /// assets/images/core/page_under_build.jpg
+  static const String pageUnderBuild =
+      'assets/images/core/page_under_build.jpg';
+
+  /// Assets for CustomerBigNavBarDark
+  /// assets/images/customer/big_nav_bar_dark.png
+  static const String bigNavBarDark =
+      'assets/images/customer/big_nav_bar_dark.png';
+
+  /// Assets for CustomerBigNavBarLight
+  /// assets/images/customer/big_nav_bar_light.png
+  static const String bigNavBarLight =
+      'assets/images/customer/big_nav_bar_light.png';
+
+  /// Assets for CustomerHomeBgDark
+  /// assets/images/customer/home_bg_dark.png
+  static const String homeBgDark = 'assets/images/customer/home_bg_dark.png';
+
+  /// Assets for CustomerHomeBgLight
+  /// assets/images/customer/home_bg_light.png
+  static const String homeBgLight = 'assets/images/customer/home_bg_light.png';
+
+  /// Assets for CustomerUser
+  /// assets/images/customer/user.png
+  static const String user = 'assets/images/customer/user.png';
+
+  /// Assets for LogoLogoDark1
+  /// assets/images/logo/logo_dark_1.png
+  static const String logoDark1 = 'assets/images/logo/logo_dark_1.png';
+
+  /// Assets for LogoLogoLight1
+  /// assets/images/logo/logo_light_1.png
+  static const String logoLight1 = 'assets/images/logo/logo_light_1.png';
+
+  /// Assets for LogoSplash12
+  /// assets/images/logo/splash_12.png
+  static const String splash12 = 'assets/images/logo/splash_12.png';
+
+  /// Assets for SvgArrowBack
+  /// assets/images/svg/arrow_back.svg
+  static const String svgArrowBack = 'assets/images/svg/arrow_back.svg';
+
+  /// Assets for SvgBuildDeveloper
+  /// assets/images/svg/build_developer.svg
+  static const String svgBuildDeveloper =
+      'assets/images/svg/build_developer.svg';
+
+  /// Assets for SvgBuildVersion
+  /// assets/images/svg/build_version.svg
+  static const String svgBuildVersion = 'assets/images/svg/build_version.svg';
+
+  /// Assets for SvgCarShop
+  /// assets/images/svg/car_shop.svg
+  static const String svgCarShop = 'assets/images/svg/car_shop.svg';
+
+  /// Assets for SvgCategoriesTapIcon
+  /// assets/images/svg/categories_tap_icon.svg
+  static const String svgCategoriesTapIcon =
+      'assets/images/svg/categories_tap_icon.svg';
+
+  /// Assets for SvgDarkMode
+  /// assets/images/svg/dark_mode.svg
+  static const String svgDarkMode = 'assets/images/svg/dark_mode.svg';
+
+  /// Assets for SvgFavouriteTabIcon
+  /// assets/images/svg/favourite_tab_icon.svg
+  static const String svgFavoriteTabIcon =
+      'assets/images/svg/favourite_tab_icon.svg';
+
+  /// Assets for SvgHomeTabIcon
+  /// assets/images/svg/home_tab_icon.svg
+  static const String svgHomeTabIcon = 'assets/images/svg/home_tab_icon.svg';
+
+  /// Assets for SvgLanguage
+  /// assets/images/svg/language.svg
+  static const String svgLanguage = 'assets/images/svg/language.svg';
+
+  /// Assets for SvgLogout
+  /// assets/images/svg/logout.svg
+  static const String svgLogout = 'assets/images/svg/logout.svg';
+
+  /// Assets for SvgNotificationIcon
+  /// assets/images/svg/notification_icon.svg
+  static const String svgNotificationIcon =
+      'assets/images/svg/notification_icon.svg';
+
+  /// Assets for SvgProfileTabIcon
+  /// assets/images/svg/profile_tab_icon.svg
+  static const String svgProfileTabIcon =
+      'assets/images/svg/profile_tab_icon.svg';
+
+  /// Assets for SvgSearch
+  /// assets/images/svg/search.svg
+  static const String svgSearch = 'assets/images/svg/search.svg';
+}
