06e08ee89a739436d60a3be792e0dc77fd49a39c
osamasabry9 <osamasabry324@gmail.com>
2024-07-13 18:47:06 +0300

    Add AppBlocObserver and SharedPref classes
    Added AppBlocObserver class to observe BLoC state changes and SharedPref class
    to handle SharedPreferences operations. Also initialized AppBlocObserver in the
    main function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
+import 'package:digital_boutique/app/bloc_observer.dart';
 import 'package:digital_boutique/app/digital_boutique_app.dart';
 import 'package:digital_boutique/app/env.variables.dart';
 import 'package:digital_boutique/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
-  // to initialize environment variables 
+  // to initialize environment variables
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // to initialize environment variables
   await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
   // to initialize firebase app in the main function
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+// to initialize bloc observer to show the state changes in the app
+  Bloc.observer = AppBlocObserver();
 /* 
    To allow the app to rotate to landscape mode when the device is
    in portrait mode and vice versa in the main function. 
