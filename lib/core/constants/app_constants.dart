0f5eeee9ffb5790099578757774f9bb5834c5040
osamasabry9 <osamasabry324@gmail.com>
2024-07-16 18:59:50 +0300

    Add user role URL constant and error helper class
    Added a constant for the user role URL and an error helper class with a default error message.

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,4 +2,6 @@ class AppConstants {
   static const String appName = 'Digital Boutique';
 static  const String baseUrl = 'https://api.escuelajs.co';
 static const String graphql = '/graphql';
+
+  static const String userRoleUrl = '/api/v1/auth/profile';
 }
