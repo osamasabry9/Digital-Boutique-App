b758585dfc74a8cf6442ea7e31ae80b60f0e914a
osamasabry9 <osamasabry324@gmail.com>
2024-07-16 21:31:49 +0300

    Add toast functionality, route updates, and shared preferences flags
    - Added showToastErrorTop and showToastSuccessTop functions for displaying
      toasts.
    - Updated routes for admin and user home screens.
    - Added flags for checking user login status and admin status in shared
      preferences.

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,4 +4,6 @@ static  const String baseUrl = 'https://api.escuelajs.co';
 static const String graphql = '/graphql';
 
   static const String userRoleUrl = '/api/v1/auth/profile';
+
+  
 }
--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -3,9 +3,11 @@ import 'package:digital_boutique/core/common/screens/no_network_screen.dart';
 import 'package:digital_boutique/core/common/screens/under_build_screen.dart';
 import 'package:digital_boutique/core/routes/base_routes_animation.dart';
 import 'package:digital_boutique/core/routes/routes.dart';
+import 'package:digital_boutique/features/admin/presentaion/home_screen_admin.dart';
 import 'package:digital_boutique/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:digital_boutique/features/auth/presentation/screens/login_screen.dart';
 import 'package:digital_boutique/features/auth/presentation/screens/sign_uo_screen.dart';
+import 'package:digital_boutique/features/customer/presentation/home_screen_user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -24,6 +26,11 @@ class AppRouter {
         );
       case Routes.signUpScreen:
         return BaseRouteAnimation(page: const SignUpScreen());
+
+      case Routes.homeScreenAdmin:
+          return BaseRouteAnimation(page: const HomeScreenAdmin());
+      case Routes.homeScreenUser:
+          return BaseRouteAnimation(page: const HomeScreenUser());
       default:
         return BaseRouteAnimation(page: const PageUnderBuildScreen());
     }
