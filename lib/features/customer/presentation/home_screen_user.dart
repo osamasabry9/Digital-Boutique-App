2ce63d31eb80aaaee1dd427f91e48505470d025a
osamasabry9 <osamasabry324@gmail.com>
2024-07-16 21:32:41 +0300

    Test home screen components for admin and user
    Create new components for the home screen for admin and user in their respective
    directories. Each component includes a Scaffold with a centered Text widget.

---
+++ lib/features/customer/presentation/home_screen_user.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenUser extends StatelessWidget {
+  const HomeScreenUser({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('HomeScreenUser')),
+    );
+  }
+}
