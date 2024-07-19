218e57bb78da6669f341982c0d4eaf98d2701418
osamasabry9 <osamasabry324@gmail.com>
2024-07-19 19:32:51 +0300

    Add admin drawer list, drawer item model, and app logout helper
    Add functionality to create a list of drawer items for the admin panel,
    introduce a model for drawer items, and implement a helper class for
    logging out the user from the application.

---
+++ lib/core/utils/admin_drawer/drawer_item_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DrawerItemModel {
+  DrawerItemModel({
+    required this.icon,
+    required this.title,
+    required this.page,
+  });
+
+  final Icon icon;
+  final Widget title;
+  final Widget page;
+}
