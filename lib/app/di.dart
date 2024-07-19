a02fe9c7f7a3fe3e0fe2b565c6ab1396c224f00b
osamasabry9 <osamasabry324@gmail.com>
2024-07-19 21:21:54 +0300

    Add dashboard blocs and repositories initialization
    Initialize dashboard blocs and repositories in the DI setup function for
    admin dashboard features. This includes categories, products, and users
    number blocs along with their respective repositories.

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -4,6 +4,11 @@ import 'package:digital_boutique/core/networking/dio_factory.dart';
 import 'package:digital_boutique/core/service/upload_image/cubit/upload_image_cubit.dart';
 import 'package:digital_boutique/core/service/upload_image/data_source/upload_image_data_source.dart';
 import 'package:digital_boutique/core/service/upload_image/repo/upload_image_repo.dart';
+import 'package:digital_boutique/features/admin/dashboard/data/data_source/dashboard_data_source.dart';
+import 'package:digital_boutique/features/admin/dashboard/data/repos/dashboard_repo.dart';
+import 'package:digital_boutique/features/admin/dashboard/presentation/bloc/categories_number/categories_number_bloc.dart';
+import 'package:digital_boutique/features/admin/dashboard/presentation/bloc/products_number/products_number_bloc.dart';
+import 'package:digital_boutique/features/admin/dashboard/presentation/bloc/users_number/users_number_bloc.dart';
 import 'package:digital_boutique/features/auth/data/data_source/auth_data_source.dart';
 import 'package:digital_boutique/features/auth/data/reposatory/auth_repos.dart';
 import 'package:digital_boutique/features/auth/presentation/bloc/auth_bloc.dart';
@@ -15,6 +20,7 @@ final getIt = GetIt.instance;
 Future<void> setupGetIt() async {
   await _initCore();
   await _initAuth();
+  await _initDashBoard();
 }
 
 /// Initializes the core functionality by creating an instance of Dio using DioFactory,
@@ -39,4 +45,13 @@ Future<void> _initAuth() async {
     ..registerFactory(() => AuthBloc(getIt<AuthRepos>()))
     ..registerLazySingleton(() => AuthRepos(getIt<AuthDataSource>()))
     ..registerLazySingleton(() => AuthDataSource(getIt<ApiService>()));
+}
+
+Future<void> _initDashBoard() async {
+  getIt
+    ..registerFactory(() => CategoriesNumberBloc(getIt<DashBoardRepo>()))
+    ..registerFactory(() => ProductsNumberBloc(getIt<DashBoardRepo>()))
+    ..registerFactory(() => UsersNumberBloc(getIt<DashBoardRepo>()))
+    ..registerLazySingleton(() => DashBoardRepo(getIt<DashBoardDataSource>()))
+    ..registerLazySingleton(() => DashBoardDataSource(getIt<ApiService>()));
 }
