38de899035fde8f53e56c7e4e4c942d8eba97d7b
osamasabry9 <osamasabry324@gmail.com>
2024-07-23 19:17:20 +0300

    Add create category functionality
    Added the ability to create a new category in the admin section by
    implementing the createCategory method in both the data source and repository
    classes. This feature allows users to add new categories to the system.

--- lib/features/admin/add_categories/data/data_source/categories_admin_data_source.dart
+++ lib/features/admin/add_categories/data/data_source/categories_admin_data_source.dart
@@ -1,5 +1,7 @@
 import 'package:digital_boutique/core/networking/api_service.dart';
 import 'package:digital_boutique/core/networking/graphql_queries/admin/categories_queries.dart';
+import 'package:digital_boutique/features/admin/add_categories/data/models/create_category_request_body.dart';
+import 'package:digital_boutique/features/admin/add_categories/data/models/create_category_response.dart';
 import 'package:digital_boutique/features/admin/add_categories/data/models/get_all_categories_response.dart';
 
 class CategoriesAdminDataSource {
@@ -9,6 +11,14 @@ class CategoriesAdminDataSource {
   Future<CategoriesGetAllResponse> getAllCategories() async {
     final response = await _graphql
         .getAllCategories(CategoriesQueries().getAllCategoriesMapQuery());
+    return response;
+  }
+
+  Future<CreateCategoryResponse> createCategory({
+    required CreateCategoryRequestBody body,
+  }) async {
+    final response = await _graphql
+        .createCategory(CategoriesQueries().createCategoryMapQuery(body: body));
     return response;
   }
 }
--- lib/features/admin/add_categories/data/repos/categories_admin_repos.dart
+++ lib/features/admin/add_categories/data/repos/categories_admin_repos.dart
@@ -1,6 +1,8 @@
 import 'package:digital_boutique/core/networking/api_result.dart';
 import 'package:digital_boutique/core/utils/error/error_helper.dart';
 import 'package:digital_boutique/features/admin/add_categories/data/data_source/categories_admin_data_source.dart';
+import 'package:digital_boutique/features/admin/add_categories/data/models/create_category_request_body.dart';
+import 'package:digital_boutique/features/admin/add_categories/data/models/create_category_response.dart';
 import 'package:digital_boutique/features/admin/add_categories/data/models/get_all_categories_response.dart';
 
 class CategoriesAdminRepos {
@@ -13,6 +15,16 @@ class CategoriesAdminRepos {
   Future<ApiResult<CategoriesGetAllResponse>> getAllCategories() async {
     try {
       final response = await _dataSource.getAllCategories();
+      return ApiResult.success(response);
+    } catch (error) {
+      return const ApiResult.failure(ErrorHelper.errorMessage);
+    }
+  }
+
+  // Create Category
+  Future<ApiResult<CreateCategoryResponse>> createCategory(CreateCategoryRequestBody requestBody) async {
+    try {
+      final response = await _dataSource.createCategory(body: requestBody);
       return ApiResult.success(response);
     } catch (error) {
       return const ApiResult.failure(ErrorHelper.errorMessage);
