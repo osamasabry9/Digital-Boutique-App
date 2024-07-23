ba83721104a5f37035a8c368d4ce8ac15d2a62aa
osamasabry9 <osamasabry324@gmail.com>
2024-07-23 16:21:47 +0300

    Add feature to fetch all categories in ApiService
    Added a new method in ApiService to fetch all categories from the server using
    GraphQL queries. This method will return a response with category details
    including id, name, and image.

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -1,5 +1,6 @@
 import 'package:digital_boutique/core/constants/app_constants.dart';
 import 'package:digital_boutique/core/service/upload_image/model/upload_image_response.dart';
+import 'package:digital_boutique/features/admin/add_categories/data/models/get_all_categories_response.dart';
 import 'package:digital_boutique/features/admin/dashboard/data/models/categories_number_response.dart';
 import 'package:digital_boutique/features/admin/dashboard/data/models/products_number_response.dart';
 import 'package:digital_boutique/features/admin/dashboard/data/models/users_number_response.dart';
@@ -48,6 +49,11 @@ abstract class ApiService {
 
   @POST(graphql)
   Future<UsersNumberResponse> numberOfUsers(
+    @Body() Map<String, dynamic> query,
+  );
+
+  @POST(graphql)
+  Future<CategoriesGetAllResponse> getAllCategories(
     @Body() Map<String, dynamic> query,
   );
 }
---
+++ lib/core/networking/graphql_queries/admin/categories_queries.dart
@@ -0,0 +1,25 @@
+
+class CategoriesQueries {
+  factory CategoriesQueries() {
+    return _instance;
+  }
+  const CategoriesQueries._();
+
+  static const CategoriesQueries _instance = CategoriesQueries._();
+
+//Categories get all
+  Map<String, dynamic> getAllCategoriesMapQuery() {
+    return {
+      'query': '''
+            {
+            categories{
+              id
+              name
+              image
+            }
+          }
+      ''',
+    };
+  }
+
+}
