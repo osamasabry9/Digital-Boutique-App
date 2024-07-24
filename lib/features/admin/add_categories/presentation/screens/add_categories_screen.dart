66e4273fff580687b31dc2618d58823b864360ac
osamasabry9 <osamasabry324@gmail.com>
2024-07-25 00:45:50 +0300

    Add DeleteCategoryBloc to AddCategoriesScreen
    Imported DeleteCategoryBloc and added it to the providers list in
    AddCategoriesScreen for future use in managing category deletion.

--- lib/features/admin/add_categories/presentation/screens/add_categories_screen.dart
+++ lib/features/admin/add_categories/presentation/screens/add_categories_screen.dart
@@ -1,6 +1,7 @@
 import 'package:digital_boutique/app/di.dart';
 import 'package:digital_boutique/core/common/widgets/admin_app_bar.dart';
 import 'package:digital_boutique/core/style/colors/colors_dark.dart';
+import 'package:digital_boutique/features/admin/add_categories/presentation/bloc/delete_category/delete_category_bloc.dart';
 import 'package:digital_boutique/features/admin/add_categories/presentation/bloc/get_all_admin_categories/get_all_admin_categories_bloc.dart';
 import 'package:digital_boutique/features/admin/add_categories/presentation/widgets/add_categories_body.dart';
 import 'package:flutter/material.dart';
@@ -15,17 +16,20 @@ class AddCategoriesScreen extends StatelessWidget {
       providers: [
         BlocProvider(
           create: (context) => getIt<GetAllAdminCategoriesBloc>()
-            ..add(const GetAllAdminCategoriesEvent.fetchAdminCategories(isNotLoading: true)),
+            ..add(
+              const GetAllAdminCategoriesEvent.fetchAdminCategories(
+                  isNotLoading: true),
+            ),
         ),
-        
+        BlocProvider(create: (context) => getIt<DeleteCategoryBloc>()),
       ],
       child: const Scaffold(
-         backgroundColor: ColorsDark.mainColor,
-          appBar: AdminAppBar(
-            title: 'Add Categories',
-            isMain: true,
-            backgroundColor: ColorsDark.mainColor,
-          ),
+        backgroundColor: ColorsDark.mainColor,
+        appBar: AdminAppBar(
+          title: 'Add Categories',
+          isMain: true,
+          backgroundColor: ColorsDark.mainColor,
+        ),
         body: AddCategoriesBody(),
       ),
     );
