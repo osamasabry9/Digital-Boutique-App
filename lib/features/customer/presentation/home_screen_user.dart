bd6bc972bf75049c0c942f6ea693199b590b80a7
osamasabry9 <osamasabry324@gmail.com>
2024-10-06 23:24:38 +0400

    Refactor home_screen_user.dart for improved readability
    Move AppLogout import and rearrange button order for better organization and clarity.

--- lib/features/customer/presentation/home_screen_user.dart
+++ lib/features/customer/presentation/home_screen_user.dart
@@ -1,7 +1,8 @@
 import 'package:digital_boutique/core/service/push_notification/notification_help.dart';
-import 'package:digital_boutique/core/utils/helpers/app_logout.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+
+import '../../../core/utils/helpers/app_logout.dart';
 
 class HomeScreenUser extends StatelessWidget {
   const HomeScreenUser({super.key});
@@ -11,23 +12,30 @@ class HomeScreenUser extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Customer')),
       body: Center(
-          child: Column(
-        children: [
-          TextButton(
-            onPressed: () {
-              NotificationHelp().getAccessToken();
-            },
-            child: const Text('Logout'),
-          ),
-          TextButton(
-            onPressed: () async {
-              final token = await FirebaseMessaging.instance.getToken();
-              print('Token customer: $token');
-            },
-            child: const Text('Get Token'),
-          ),
-        ],
-      )),
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                AppLogout().logout();
+              },
+              child: const Text('Logout'),
+            ),
+            TextButton(
+              onPressed: () {
+                NotificationHelp().getAccessToken();
+              },
+              child: const Text('Get Token'),
+            ),
+            TextButton(
+              onPressed: () async {
+                final token = await FirebaseMessaging.instance.getToken();
+                print('Token customer: $token');
+              },
+              child: const Text('Get Firebase Token'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
